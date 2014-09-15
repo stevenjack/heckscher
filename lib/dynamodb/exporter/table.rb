58574c18b13320b7f8723ccee9463c700cea4863
Robert Kenny <kenoir@gmail.com>
2014-09-15 18:47:15 +0100

    add export class skeleton

--- lib/dynamodb/exporter/table.rb
+++ lib/dynamodb/exporter/table.rb
@@ -1,8 +1,49 @@
 module Dynamodb
   module Exporter
-    class Table
+    class Export
 
-      def initialize(name)
+      attr_reader :table_name, :contents, :limit
+
+      def initialize(table_name, limit)
+        @table_name = table_name
+        @limit      = limit
+        @ddb        = AWS::DynamoDB::Client.new(:api_version => '2011-12-05')
+
+        @contents   = []
+        @start_key  = nil
+      end
+
+      def run
+        while @start_key do
+          scan.tap do |s|
+            @contents.push contents_from(s)
+            @start_key = last_key_from s
+          end
+        end
+      end
+
+      protected
+
+      def scan
+        @ddb.scan options_with(limit, start_key)
+      end
+
+      def last_key_from(scan)
+        # do work
+        last_evaluated_key
+      end
+
+      def contents_from(scan)
+        # do work
+        contents
+      end
+
+      def options_with(limit, exclusive_start_key || 0)
+        # setup options
+        {
+          :foo => 'bar'
+        }
+      end
 
     end
   end
