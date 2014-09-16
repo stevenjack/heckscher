c3c27b8506d72a9d32f565c11790ca7a272f0b24
Robert Kenny <kenoir@gmail.com>
2014-09-16 16:23:25 +0100

    reading from table

--- lib/dynamodb/exporter/export.rb
+++ lib/dynamodb/exporter/export.rb
@@ -1,3 +1,5 @@
+require 'aws-sdk'
+
 module Dynamodb
   module Exporter
     class Export
@@ -7,16 +9,16 @@ module Dynamodb
       def initialize(table_name, limit)
         @table_name = table_name
         @limit      = limit
-        @ddb        = AWS::DynamoDB::Client.new(:api_version => '2011-12-05')
+        @ddb        = ::AWS::DynamoDB::Client.new(:api_version => '2011-12-05')
 
         @contents   = []
-        @start_key  = nil
+        @start_key  = true
       end
 
       def run
         while @start_key do
           scan.tap do |s|
-            @contents.push contents_from(s)
+            @contents << contents_from(s)
             @start_key = last_key_from s
           end
         end
@@ -27,24 +29,24 @@ module Dynamodb
       protected
 
       def scan
-        @ddb.scan options_with(limit, start_key)
+        @ddb.scan options_with(limit, @start_key)
       end
 
       def last_key_from(scan)
-        # do work
-        last_evaluated_key
+        scan["LastEvaluatedKey"]["HashKeyElement"]
       end
 
       def contents_from(scan)
-        # do work
-        contents
+        scan["Items"]
       end
 
-      def options_with(limit, exclusive_start_key || 0)
-        # setup options
+      def options_with(limit, start_key = nil)
         {
-          :foo => 'bar'
-        }
+          :table_name => table_name,
+          :limit => limit,
+        }.tap do |o|
+          o[:exclusive_start_key] = start_key if start_key.is_a? Hash
+        end
       end
 
     end
